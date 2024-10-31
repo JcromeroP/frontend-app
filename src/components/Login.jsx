61c4b8db1230e4736c26e7aa0386dd3e177a5426
jaiberleon <jaleong-2021a@corhuila.edu.co>
2024-10-30 19:49:15 -0500

    cambio rutas

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,109 +2,80 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Registro from './Registro'
+import Registro from './Registro';
 import logoCorhuila from '../logoCorhuila.jpg';
 import fondo from '../Fondo.png';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import { jwtDecode } from 'jwt-decode'; 
-import { Button, Form, FormGroup, Container, Card, CardBody, FormFeedback, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Container, Card, CardBody, Input } from 'reactstrap';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [validated, setValidated] = useState(false);
-  const [errors, setErrors] = useState({ username: '', password: '' });
   const navigate = useNavigate();
-
-  const validateForm = () => {
-    let formIsValid = true;
-    let newErrors = { username: '', password: '' };
-
-    // Validación del nombre de usuario
-    if (!username) {
-      formIsValid = false;
-      newErrors.username = 'El nombre de usuario es requerido.';
-    }
-
-    // Validación de la contraseña
-    if (!password) {
-      formIsValid = false;
-      newErrors.password = 'La contraseña es requerida.';
-    } else if (password.length < 6) {
-      formIsValid = false;
-      newErrors.password = 'La contraseña debe tener al menos 6 caracteres.';
-    }
-
-    setErrors(newErrors);
-    return formIsValid;
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-      try {
-        const loginData = {
-          username: username,
-          password: password,
-        };
-/* http://ec2-18-189-202-101.us-east-2.compute.amazonaws.com:8081*/
-        const response = await axios.post('http://localhost:8080/api/login', loginData, {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
+    try {
+      const loginData = {
+        email: username,
+        password: password,
+      };
+      /* http://ec2-18-189-202-101.us-east-2.compute.amazonaws.com:8081*/
+      const response = await axios.post('http://localhost:8080/api/user/login', loginData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-        const { status, message, data } = response.data;
-        
-        if (response.data.status === 200 || response.data.status === 201) {
-         
-          decodeToken(response.data.data)
-      }
-      if(response.data.status === 400 || response.data.status === 403 ||response.data.status === 401 || response.data.status === 404  ){
-          Swal.fire({
-              icon: 'error',
-              title: 'Error En el inicio de sesion',
-              text: message || 'Por favor inténtalo nuevamente.',
-              confirmButtonText: 'Aceptar'
-          })
-      }
-      } catch (error) {
+      const { status, message, data } = response.data;
+      
+      if (status === 200 || status === 201) {
+        decodeToken(data);
+      } else if ([400, 403, 401, 404].includes(status)) {
         Swal.fire({
           icon: 'error',
-          title: 'Error en el servidor',
-          text: error.response?.data?.message || 'Ocurrió un error al intentar iniciar sesión.',
-          confirmButtonText: 'Aceptar'
+          title: 'Error en el inicio de sesión',
+          text: message || 'Por favor inténtalo nuevamente.',
+          confirmButtonText: 'Aceptar',
         });
       }
-    
+    } catch (error) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error en el servidor',
+        text: error.response?.data?.message || 'Ocurrió un error al intentar iniciar sesión.',
+        confirmButtonText: 'Aceptar',
+      });
+    }
   };
 
   const decodeToken = (token) => {
     const decodedToken = jwtDecode(token);
     const userRole = decodedToken.role;
 
-    localStorage.setItem('idUser',decodedToken.idUser);
-    localStorage.setItem('name',decodedToken.name);
-    localStorage.setItem('facultad',decodedToken.facultad);
-    localStorage.setItem('programa',decodedToken.programa);
-    localStorage.setItem('email',decodedToken.email);
-    localStorage.setItem('role',decodedToken.role);
+    localStorage.setItem('idUser', decodedToken.idUser);
+    localStorage.setItem('name', decodedToken.name);
+    localStorage.setItem('facultad', decodedToken.facultad);
+    localStorage.setItem('programa', decodedToken.programa);
+    localStorage.setItem('email', decodedToken.email);
+    localStorage.setItem('role', decodedToken.role);
 
-   if(userRole === 'Docente'){
-    navigate("/AgendasDocente"); 
-   }
-   if(userRole === 'Admin'){
-    navigate("/Administracion");
-   }
-   if(userRole === 'Decano'){
-    navigate("/Decano/Autorizacion_Decano");
-   }
-   if(userRole === 'Director De programa'){
-    navigate("/DirectorPrograma");
-   }
+    if (userRole === 'Docente') {
+      navigate("/AgendasDocente"); 
+    }
+    if (userRole === 'Admin') {
+      navigate("/Administracion");
+    }
+    if (userRole === 'Decano') {
+      navigate("/Decano/Autorizacion_Decano");
+    }
+    if (userRole === 'Director De programa') {
+      navigate("/DirectorPrograma");
+    }
   };
-
 
   return (
     <div className="login-page" style={styles.page}>
@@ -114,7 +85,7 @@ const Login = () => {
             <div style={styles.logoContainer}>
               <img className='logo-login' src={logoCorhuila} alt="CORHUILA Logo" />
             </div>
-            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form onSubmit={handleSubmit}>
               <FormGroup>
                 <Input 
                   type="email" 
@@ -122,10 +93,8 @@ const Login = () => {
                   id="username" 
                   placeholder="Correo electrónico" 
                   value={username} 
-                  onChange={(e) => setUsername(e.target.value)} 
-                  invalid={!!errors.username}
+                  onChange={(e) => setUsername(e.target.value)}
                 />
-                <FormFeedback>{errors.username}</FormFeedback>
               </FormGroup>
               <FormGroup>
                 <Input 
@@ -134,14 +103,14 @@ const Login = () => {
                   id="inputPassword" 
                   placeholder="Contraseña" 
                   value={password} 
+                  onChange={(e) => setPassword(e.target.value)}
                 />
-                <FormFeedback>{errors.password}</FormFeedback>
               </FormGroup>
-              <Button  type="submit" color="success">
+              <Button type="submit" color="success">
                 Acceder
               </Button>
               <div className="mt-3">
-                <a href="/forgot-password" style={{...styles.link, display: 'block'}  }>
+                <a href="/forgot-password" style={{ ...styles.link, display: 'block' }}>
                   ¿Olvidó su contraseña?
                 </a>
               </div>
@@ -152,8 +121,6 @@ const Login = () => {
     </div>
   );
 };
-
-
 
 const styles = {
   page: {
